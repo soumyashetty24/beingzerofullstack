8a92a887ef4acffb4f44a34438cb3300862e090b
Your Name <you@example.com>
2020-01-24 14:00:43 +0530

    changes for port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port =app.env.PORT ||3000
 const path=require('path')
 app.use(express.static(__dirname+'/dist/angularproject1'))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/dist/angularproject1/index.html')))
